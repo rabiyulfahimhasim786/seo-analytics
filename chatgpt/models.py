0d7faf267c23fc6e09c335b5431f81771d6cb221
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2023-05-22 07:08:16 -0500

    Add files via upload

--- chatgpt/models.py
+++ chatgpt/models.py
@@ -6,3 +6,8 @@ from django.db import models
 class Chat(models.Model):
     input_text = models.TextField()
     output_text = models.TextField()
+
+
+class Gpt(models.Model):
+    input_query = models.TextField()
+    output_query = models.TextField()
--- chatgpt/urls.py
+++ chatgpt/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import (ChatView, PdfUpdateDeleteApiView)
+from .views import (ChatView, ChatUpdateDeleteApiView, GptView, GptUpdateDeleteApiView)
 from . import views
+# from .views import CustomChatView
 
 urlpatterns = [
-    path('chatgpt/', views.chatgptindex, name='chatgptindex'),
+    # path('chatgpt/', views.ChatView,  name='chatgpt'),
+    path('', views.index,  name='index'),
     path('chat/', ChatView.as_view()),
-    path('chat/<int:pk>/', PdfUpdateDeleteApiView.as_view()),
+    path('chat/<int:pk>/', ChatUpdateDeleteApiView.as_view()),
+    # path('myview/', CustomChatView.as_view(), name='my-view'),
+    path('gpt/', GptView.as_view()),
+    path('gpt/<int:pk>/', GptUpdateDeleteApiView.as_view()),
 ]
-
